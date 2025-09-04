1003de3e5f816a2dfb3ccbd2736b6b1eae3b7ad1
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 16:20:32 +0000

    BUILD: reinicio completo con estructura modular  organizada

---
+++ modulo_3/glossary.py
@@ -0,0 +1,11 @@
+# 6-3: Glossary
+glossary = {
+    "variable": "A name that stores a value.",
+    "loop": "A way to repeat actions.",
+    "function": "Reusable block of code.",
+    "list": "Ordered collection of items.",
+    "dictionary": "Collection of key-value pairs."
+}
+
+for word, meaning in glossary.items():
+    print(f"{word}:\n  {meaning}\n")
