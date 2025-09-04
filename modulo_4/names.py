5beb80b666efe50810b2d1250373477432c3665e
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 19:14:13 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_4/names.py
@@ -0,0 +1,11 @@
+# 3-1: Names
+names = ["Laufey", "Diana", "Itsuko", "Airi"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+#You can also use a loop to make it cleaner
+# for name in names:
+    #print(name)
