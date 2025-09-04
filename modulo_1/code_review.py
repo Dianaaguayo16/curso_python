0929377bf275a07b7ee6d8fc26fe3ae2b420d30f
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 06:40:10 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_1/code_review.py
@@ -0,0 +1,9 @@
+# Diana Aguayo – 31/08/2025
+# Prints favorite pizzas with a message for each
+
+pizzas = ["pepperoni", "hawaiian", "bbq chicken"]
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("\nI really love pizza!")
