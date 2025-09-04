87af98f476810f24da7a065054cb3ee52bb04763
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 18:08:03 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_3/hello_admin.py
@@ -0,0 +1,8 @@
+# 5-8: Hello Admin
+usernames = ["admin", "jaden", "luna", "mario", "sofia"]
+
+for user in usernames:
+    if user == "admin":
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {user.title()}, thank you for logging in again.")
