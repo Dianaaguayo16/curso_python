9fb3bedd27b9b4227f888e4e7e62211173f6a74e
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 18:27:01 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_3/large_shirts.py
@@ -0,0 +1,7 @@
+# 8-4: Large Shirts
+def make_shirt(size="L", message="I love Python"):
+    print(f"Making a {size} shirt with the message: '{message}'.")
+
+make_shirt()  # Large with default message
+make_shirt("M")  # Medium with default message
+make_shirt("S", "Keep coding!")  # Custom size and message
