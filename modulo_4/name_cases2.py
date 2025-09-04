4d27e7d1589c51adc944316a5fd08714867c0228
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 19:12:20 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_4/name_cases2.py
@@ -0,0 +1,14 @@
+# Autor: Diana Aguayo
+# Fecha: 31 de agosto de 2025
+# Este programa muestra un nombre en minúsculas, mayúsculas y formato título.
+
+nombre = "diana aguayo"
+
+# Imprime el nombre en minúsculas
+print(nombre.lower())
+
+# Imprime el nombre en mayúsculas
+print(nombre.upper())
+
+# Imprime el nombre con la primera letra de cada palabra en mayúscula
+print(nombre.title())
