32c1694556d6e5ef94c99496bc8628c452f448ff
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 05:37:47 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_1/admin.py
@@ -0,0 +1,14 @@
+from user import User
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age, location):
+        super().__init__(first_name, last_name, age, location)
+        self.privileges = ["can add post", "can delete post", "can ban user"]
+
+    def show_privileges(self):
+        print("Admin privileges:")
+        for privilege in self.privileges:
+            print(f"- {privilege}")
+
+admin = Admin("Diana", "Aguayo", 22, "Ciudad Guzmán")
+admin.show_privileges()
