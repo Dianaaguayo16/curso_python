8077d818e0b9b1ae9e1f985282935a56344ad21a
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 06:33:11 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_1/cities2.py
@@ -0,0 +1,7 @@
+# 8-5: Cities
+def describe_city(city, country="Iceland"):
+    print(f"{city.title()} is in {country.title()}.")
+
+describe_city("Reykjavik")
+describe_city("Kyoto", "Japan")
+describe_city("Venice", "Italy")
