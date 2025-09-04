bb1fc1240b23cb2bcb4044ed76e1db4b248e1b8d
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 15:54:26 +0000

    BUILD: reinicio completo con estructura modular  organizada

---
+++ modulo_3/favorite_numbers.py
@@ -0,0 +1,11 @@
+# 6-2: Favorite Numbers
+favorite_numbers = {
+    "Ana": 7,
+    "Luis": 3,
+    "Carlos": 9,
+    "María": 5,
+    "Diana": 1
+}
+
+for name, number in favorite_numbers.items():
+    print(f"{name}'s favorite number is {number}.")
