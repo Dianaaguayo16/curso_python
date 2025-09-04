be96a71e2c61787bb5526c9b75f4a2a89d9af430
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 15:41:13 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_2/favorite_number2.py
@@ -0,0 +1,11 @@
+# Autor: Diana Aguayo
+# Fecha: 31 de agosto de 2025
+# Este programa guarda tu número favorito en una variable y lo muestra en un mensaje.
+
+favorite_number = 7
+
+# Crea un mensaje que incluye el número favorito
+message = f"My favorite number is {favorite_number} because it feels lucky!"
+
+# Imprime el mensaje
+print(message)
