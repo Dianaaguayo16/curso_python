5daa66c33068a1cc15017762a615a88ccd3e37c0
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 06:38:07 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_1/city_names.py
@@ -0,0 +1,7 @@
+# 8-6: City Names
+def city_country(city, country):
+    return f"{city.title()}, {country.title()}"
+
+print(city_country("santiago", "chile"))
+print(city_country("paris", "france"))
+print(city_country("kyoto", "japan"))
