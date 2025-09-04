2d1d2362132eb1004b67e006fa401f877c366932
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 05:51:42 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_1/animals.py
@@ -0,0 +1,7 @@
+# 4-2: Animals
+animals = ["dog", "cat", "rabbit"]
+
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print("\nAny of these animals would make a great pet!")
