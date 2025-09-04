a9b02c344c117dfdc7c402a6c417e6af9a89ec9a
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 05:44:40 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_1/alien_colors.py
@@ -0,0 +1,12 @@
+# 5-3: Alien Colors #1
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("You just earned 5 points!")
+
+# Version that fails (no output)
+alien_color = "red"
+
+if alien_color == "green":
+    print("You just earned 5 points!")  # No outputtree
