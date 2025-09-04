ddcd6738c3e38981f10ff7e2d552dbebc8963a76
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 19:20:25 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_4/number_eight.py
@@ -0,0 +1,5 @@
+# 2-9: Number Eight
+print(5 + 3)     # Addition
+print(10 - 2)    # Subtraction
+print(4 * 2)     # Multiplication
+print(16 / 2)    # Division The division result will be 8.0 because Python returns a float when using /
