43843a8c09a1f195bef9736faab0ce68e9a66c02
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-04 15:22:58 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_2/favorite_fruit.py
@@ -0,0 +1,18 @@
+# 5-7: Favorite Fruit
+
+favorite_fruits = ["banana", "mango", "strawberry"]
+
+if "banana" in favorite_fruits:
+    print("You really like bananas!")
+
+if "mango" in favorite_fruits:
+    print("You really like mangoes!")
+
+if "strawberry" in favorite_fruits:
+    print("You really like strawberries!")
+
+if "apple" in favorite_fruits:
+    print("You really like apples!")
+
+if "kiwi" in favorite_fruits:
+    print("You really like kiwis!")
