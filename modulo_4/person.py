ad24f78decd7710c3085c8d5f4369aba8aeaee50
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-05 15:48:00 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_4/person.py
@@ -0,0 +1,12 @@
+# 6-1: Person
+person = {
+    "first_name": "Diana",
+    "last_name": "Aguayo",
+    "age": 21,
+    "city": "Ciudad Guzmán"
+}
+
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
