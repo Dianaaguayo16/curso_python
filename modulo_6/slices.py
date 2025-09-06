0709a6fe3c606bae6268cff95eebbea2275f2130
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-06 22:58:44 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_6/slices.py
@@ -0,0 +1,6 @@
+# 4-10: Slices
+languages = ["Python", "JavaScript", "C++", "Rust", "Go", "Ruby"]
+
+print("Three of the first items in the list are:", languages[:3])
+print("Three items from the middle of the list are:", languages[2:5])
+print("The last three items in the list are:", languages[-3:])
