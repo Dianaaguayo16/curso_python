4ee0f145856f82a46b6837a002a98d8345ddfef5
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-07 02:57:45 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_7/three_restaurants.py
@@ -0,0 +1,10 @@
+from restaurant import Restaurant
+
+
+r1 = Restaurant("Sabor Latino", "Mexican")
+r2 = Restaurant("Tokyo Table", "Japanese")
+r3 = Restaurant("Bella Pasta", "Italian")
+
+r1.describe_restaurant()
+r2.describe_restaurant()
+r3.describe_restaurant()
