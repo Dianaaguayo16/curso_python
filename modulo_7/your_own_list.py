0802a1eecf113744b39a6dc28e0702504fa1c174
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-07 03:22:02 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_7/your_own_list.py
@@ -0,0 +1,10 @@
+# 3-3: Your Own List
+transportes = ["motorcycle", "electric car", "bicycle", "drone"]
+
+for item in transportes:
+    print(f"I would like to own a {item}.")
+
+print(f"I would like to own a {transportes[0]} for weekend rides.")
+print(f"I would like to own an {transportes[1]} for daily commuting.")
+print(f"I would like to own a {transportes[2]} for exercise.")
+print(f"I would like to own a {transportes[3]} for aerial photography.")
