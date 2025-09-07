8cdb60e82eb4c53ed156e41b8a6eefed7b69363b
Dianaaguayo16 <dianasofiaaguayozuniga16@gmail.com>
2025-09-07 02:53:01 +0000

    BUILD: reinicio completo con estructura modular organizada

---
+++ modulo_7/t_shirt.py
@@ -0,0 +1,6 @@
+# 8-3: T-Shirt
+def make_shirt(size, message):
+    print(f"Making a {size} shirt with the message: '{message}'.")
+
+make_shirt("M", "Code like a girl!")  # Positional
+make_shirt(size="L", message="Python rocks!")  # Keyword
